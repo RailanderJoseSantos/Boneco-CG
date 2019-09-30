f3186cbd333a3c0e715cf52db66b5eb90e2aa2d1
Railander Jose <railandergama@gmail.com>
2019-09-30 16:19:30 -0300

    comit 01

---
+++ Assets/scripts/MovementScript.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.Serialization;
+
+namespace Player {
+    public class MovementScript : MonoBehaviour {
+        public float moveSpeed;
+        public float turnSpeed;
+        public float turnValue;
+
+        void Start() {
+        }
+
+        void Update() {
+            CaptureTranslation();
+            CaptureTurn();
+        }
+
+        private void CaptureTranslation() {
+            var zInput = Input.GetAxis("Horizontal");
+            var xInput = Input.GetAxis("Vertical");
+            var translation = Time.deltaTime * moveSpeed * new Vector3(-xInput, 0, zInput);
+            transform.Translate(translation);
+        }
+
+        private void CaptureTurn() {
+            var yTurn = Input.GetAxis("Mouse X");
+            if (yTurn == 0) return;
+            turnValue += turnSpeed * yTurn * Time.deltaTime;
+            transform.eulerAngles = new Vector3(0, turnValue, 0);
+        }
+    }
+}
