98af9b9f6c2a0a111e6060a631ba4ac36850aa74
Railander Jose <railandergama@gmail.com>
2019-10-01 16:44:31 -0300

    Fisica:
    objeto detecta contato com outro, seja por OnCollision ou Trigger

---
+++ Assets/scripts/ColisaoFisica1.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ColisaoFisica1 : MonoBehaviour
+{
+
+    Rigidbody rigidiBody;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rigidiBody = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //aplicar força ao clicar
+/*      if(Input.GetMouseButtonDown(0)){
+        rigidiBody.AddForce(transform.forward * 30, ForceMode.Impulse);
+      }   */
+    }
+
+   /*public void  OnCollisionStay(Collision outro){
+         print("Colisão comecou com " +gameObject.name);
+    }*/
+
+
+    public void  OnCollisionEnter(Collision outro){
+        print("Colisão comecou com "+gameObject.name);
+    }
+
+    /*public void  OnCollisioExit(Collision outro){    
+        print("Colisão comecou com " +gameObject.name + "acabou");
+    }*/
+
+   /*public void OnTriggerEnter(Collider outro){
+       //marcar box trigger em esfera rosa
+        print("Trigger disparada");
+    }*/
+}
